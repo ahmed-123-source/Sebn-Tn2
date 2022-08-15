52dd276f3dff57cf5c89ecf81869a05daafc2523
Ahmed <ahmed.charfeddine@esprit.tn>
2022-08-15 13:12:46 +0100

    profile utilisateur

--- src/Controller/PkController.php
+++ src/Controller/PkController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\EditProfileType;
+use App\Entity\Users;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class PkController extends AbstractController
@@ -16,8 +21,70 @@ class PkController extends AbstractController
     public function index(): Response
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
-        return $this->render('pk/index.html.twig', [
+        return $this->render('users/index.html.twig', [
             'controller_name' => 'PkController',
         ]);
     }
+
+    /**
+     * @Route("/users/profil", name="profile")
+     */
+    
+    public function edit(): Response
+    {
+       /* $this->denyAccessUnlessGranted('ROLE_USER'); */
+        return $this->render('users/profil.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route("/users/profil/modifier", name="profil_modifier")
+     */
+    public function editProfile(Request $request)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('message', 'Profil mis à jour');
+            return $this->redirectToRoute('app_pk');
+        }
+
+        return $this->render('users/edit-profile.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+     /**
+     * @Route("/users/pass/modifier", name="pass_modifier")
+     */
+    public function editPass(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        if($request->isMethod('POST')){
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $this->getUser();
+
+            // On vérifie si les 2 mots de passe sont identiques
+            if($request->request->get('pass') == $request->request->get('pass2')){
+                $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('pass')));
+                $em->flush();
+                $this->addFlash('message', 'Mot de passe mis à jour avec succès');
+
+                return $this->redirectToRoute('profile');
+            }else{
+                $this->addFlash('error', 'Les deux mots de passe ne sont pas identiques');
+            }
+        }
+
+        return $this->render('users/editpass.html.twig');
+    }
+    
+   
 }
